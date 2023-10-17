9accdfbe3a4291855d191eeb1afa4d7935cc96bc
softguidetech <softguidetech@gmail.com>
2023-10-17 16:06:27 +0400

    initial

---
+++ ecommerce_sgt/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "e-commerce sgt",
+
+    'summary': """Shopping
+    """,
+
+    'description': """
+    """,
+
+    'author': "Jamsheena kc",
+    'website': "http://www.sgt.com",
+
+    'category': 'e-commerce',
+    'assets': {
+        'web.assets_frontend': [
+            'ecommerce_sgt/static/scss/styles.scss',
+        ]
+    },
+    'version': '0.1',
+
+    'depends': ['base','portal','web','website', 'website_sale','website_blog'],
+    'data': [
+        'views/header.xml',
+        'views/footer.xml',
+        'views/home.xml',
+        'views/add_to_cart.xml',
+    ],
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3'
+}
